20b984adc222953b84064623d583b18ce88a46d4
Maxime_DO <maxigo@live.fr>
2023-01-13 14:42:30 +0100

    initialisation de Tweepy

---
+++ Tweepy/POC.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import tweepy
+
+load_dotenv()
+
+# Ajoutez vos clés d'accès à l'API Twitter ici
+consumer_key = os.getenv('API_Key')
+consumer_secret = os.getenv('API_Key_Secret')
+access_token = os.getenv('Access_Token')
+access_token_secret = os.getenv('Access_Token_Secret')
+
+
+# Authentification à l'API Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Récupération des informations d'un tweet à partir de son URL
+#url = "https://twitter.com/<username>/status/<tweet_id>"
+url = "https://twitter.com/TheGreatReview_/status/1613552566796980224"
+status = api.get_status(url.split("/")[-1])
+print(status.text)
